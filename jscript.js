dd74c875d6d893d90ba51dfaeac53dc26947444d
ywtv <127862088+ywtv@users.noreply.github.com>
2023-03-14 16:50:07 +0100

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,3 +1,3 @@
 const params = new URLSearchParams(window.location.search); //parse params
 const q = params.get("q"); //get q param
-document.getElementById("intense").value = q; //assign q value to input when loading
+document.getElementById("magnet").href = "magnet:?xt=urn:btih:" + q; //assign q value to input when loading
