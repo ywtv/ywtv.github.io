9f2b00136b7fcd66358587b87e26fe2087b8c4aa
ywtv <127862088+ywtv@users.noreply.github.com>
2023-03-14 16:38:37 +0100

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,3 +1,3 @@
 const params = new URLSearchParams(window.location.q); //parse params
 const q = params.get("q"); //get q param
-document.getElementbyid("intense").value = q; //assign q value to input when loading
+document.getElementById("intense").value = q; //assign q value to input when loading
